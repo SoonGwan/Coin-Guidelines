03a05e4b1a269ecfaad2265c0f7648626402394d
권순관 <reactsaranghe@naver.com>
2021-04-14 11:14:52 +0900

    feat(coin-line): crypto api 요청, redux 관리 (#16)

--- apps/coin-line/src/container/MainContainer/MainContainer.tsx
+++ apps/coin-line/src/container/MainContainer/MainContainer.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import Main from '../../components/Main';
+import React, { useEffect } from "react";
+import Main from "../../components/Main";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCoins, coinSelector } from "../../slices/coin";
 
 const MainContainer = () => {
+	const dispatch = useDispatch();
+	const { coins, loading, hasError } = useSelector(coinSelector);
+
+	useEffect(() => {
+		dispatch(fetchCoins());
+	}, [dispatch]);
+	console.log("obj", coins, loading, hasError);
 	return <Main />;
 };
 
