a0edb688d1a855f2a1af29e1c684dabfc39d2980
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-04-14 11:14:04 +0900

    feat(coin-line): crypto api 요청, redux 관리

--- apps/coin-line/src/main.tsx
+++ apps/coin-line/src/main.tsx
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "@primer/components";
 import App from "./components/app";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import rootReducer from "./slices";
+
+const store = configureStore({ reducer: rootReducer });
 
 ReactDOM.render(
 	<React.StrictMode>
 		<RecoilRoot>
-			<ThemeProvider>
-				<App />
-			</ThemeProvider>
+			<Provider store={store}>
+				<ThemeProvider>
+					<App />
+				</ThemeProvider>
+			</Provider>
 		</RecoilRoot>
 	</React.StrictMode>,
 	document.getElementById("root")
