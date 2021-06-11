b0656d9dd341998c4efbd29cedbb2f94b319f5f0
권순관 <reactsaranghe@naver.com>
2021-06-11 21:28:57 +0900

    fix: dollar as number

--- libs/utils/src/lib/cryptoMoney.ts
+++ libs/utils/src/lib/cryptoMoney.ts
@@ -1,22 +1,22 @@
 export const unitStrategyUS = (dollar: number) => {
-	return "$" + dollar.toLocaleString("en-US");
+  return "$" + dollar.toLocaleString("en-US");
 };
 
 export const addDollar = (money: string | number) => {
-	const dollar = Number(money);
-	return unitStrategyUS(dollar);
+  const dollar = Number(money);
+  return unitStrategyUS(dollar);
 };
 
 export const shortHitProfit = (money: string, ratio: number) => {
-	const dollar = Number(money);
-	const result = dollar * (1 + ratio / 100);
+  const dollar = Number(money);
+  const result = dollar * (1 + ratio / 100);
 
-	return unitStrategyUS(Math.floor(result));
+  return unitStrategyUS(result);
 };
 
 export const shortHitLoss = (money: string, ratio: number) => {
-	const dollar = Number(money);
-	const result = dollar * (1 - ratio / 100);
+  const dollar = Number(money);
+  const result = dollar * (1 - ratio / 100);
 
-	return unitStrategyUS(Math.floor(result));
+  return unitStrategyUS(result);
 };
--- libs/utils/src/lib/pricePercentCalc.ts
+++ libs/utils/src/lib/pricePercentCalc.ts
@@ -7,10 +7,9 @@ export const percentCalculationResult = ({
   totalValue,
   percentValue,
 }: Props) => {
-  console.log(totalValue);
   const mathematicalFormula = totalValue - totalValue * (percentValue / 100);
 
   return totalValue < 10
-    ? mathematicalFormula.toFixed(3)
-    : Math.floor(mathematicalFormula);
+    ? Number(mathematicalFormula.toFixed(3))
+    : Number(Math.floor(mathematicalFormula));
 };
