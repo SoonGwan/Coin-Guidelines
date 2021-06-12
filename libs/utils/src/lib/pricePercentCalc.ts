1b087928aee08108882da32ac95a01b8161dd107
권순관 <reactsaranghe@naver.com>
2021-06-12 22:22:02 +0900

    feat: mathematicalFormula value 0 return

--- libs/utils/src/lib/pricePercentCalc.ts
+++ libs/utils/src/lib/pricePercentCalc.ts
@@ -9,6 +9,10 @@ export const percentCalculationResult = ({
 }: Props) => {
   const mathematicalFormula = totalValue - totalValue * (percentValue / 100);
 
+  if (mathematicalFormula <= 0) {
+    return 0;
+  }
+
   return totalValue < 10
     ? Number(mathematicalFormula.toFixed(3))
     : Number(Math.floor(mathematicalFormula));
